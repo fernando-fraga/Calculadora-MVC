3d55f0e43c100560b45af73b791d7b5260fda190
Fernando Fraga Freitas <fernando.freitas@treeinova.com.br>
2023-07-28 12:55:58 -0300

    Calculadora em MVC

---
+++ calculadoraV1/Controllers/CalculadoraController.cs
@@ -0,0 +1,25 @@
+﻿using calculadoraV1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace calculadoraV1.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        public IActionResult Index() {
+            return View(new CalculadoraModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculadoraModel model) 
+        {
+            model.Resultado = model.Operador switch {
+                "+" => model.Valor1 + model.Valor2,
+                "-" => model.Valor1 - model.Valor2,
+                "*" => model.Valor1 * model.Valor2,
+                "/" => model.Valor1 / model.Valor2,
+                _ => (double?)0,
+            };
+            return View(model);
+        }
+    }
+}
---
+++ calculadoraV1/Models/CalculadoraModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace calculadoraV1.Models
+{
+    public class CalculadoraModel
+    {
+        public double? Valor1 { get;set;}
+        public double? Valor2 { get;set;}
+       
+        public string? Operador { get;set;}
+        public double? Resultado { get;set;}
+
+      
+    }
+}
